cd67a6f24df9319e476ca593248b66c7ca1a3fec
lanren <lanren@qq.com>
2021-06-04 22:11:10 +0800

    push commit

--- src/main/java/ddd/Git.java
+++ src/main/java/ddd/Git.java
@@ -8,5 +8,6 @@ public class Git {
         System.out.println("hello youdao!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
